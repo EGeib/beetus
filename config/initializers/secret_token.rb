73319b53e76320516c6d62f0ad6ddb07efb1b4d8
Eric Geib <ericgeib@gmail.com>
2011-07-08 11:37:40 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beetus::Application.config.secret_token = '0951da9d92abddc4e080a538fb036b80974fea3cc9b2c0181587c993f03d710e9d1cf579bb993a6315ccb118b131dc080c52054afb9f76111a037910c9e17868'
